c960b10f7800e5d4d827b7aa898f88a2482a4045
Weslley Gomes <wegomes@santillana.com>
2024-07-12 10:42:29 -0300

    Módulo 1 - Aula 4: Criando nossa primeira view

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,16 @@
+export class NegociacoesView {
+    template(): string {
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        `;
+    }
+}
